3b86449c61e9bddd51a67375cd1a757193fc1615
Ataago <ataago7@gmail.com>
2018-02-13 22:26:02 +0530

    adfd

--- 2018/Tower of Hanoi/Tower of Hanoi.c
+++ 2018/Tower of Hanoi/Tower of Hanoi.c
@@ -1,9 +1,23 @@
 #include<stdio.h>
 #include<conio.h>
 
-void hanoi();
+void hanoi(int n, char *source, char *auxilary, char *destination)
+{
+    if(n==0)
+        return;
 
+    hanoi(n-1,source,destination,auxilary);
+    printf("Move disc %d from %s to %s\n",n,source,destination);
+    hanoi(n-1,auxilary,source,destination);
+}
 void main()
 {
+    int n;
 
+    printf("Enter Number of Discs: ");
+    scanf("%d",&n);
+
+    printf("Objective is to move all the discs from A to C\n\n");
+
+    hanoi(n,"A","B","C");
 }
